c5e7b452bb5e176dd023c3fda909447594e0da41
claytongroth <claytoncgrth@gmail.com>
2020-11-04 13:00:20 -0700

    Added new table fields and added spaces to genres with regex

--- src/components/RestaurantsSearchPage.js
+++ src/components/RestaurantsSearchPage.js
@@ -123,6 +123,7 @@ const RestaurantsSearchPage = () => {
                         <tr>
                             <th>Name</th>
                             <th>City</th>
+                            <th>State</th>
                             <th>Genres</th>
                             <th>Attire</th>
                         </tr>
@@ -132,7 +133,8 @@ const RestaurantsSearchPage = () => {
                                     <tr key={i} >
                                         <td>{item.name}</td>
                                         <td>{item.city}</td>
-                                        <td>{item.genre}</td>
+                                        <td>{item.state}</td>
+                                        <td>{item.genre.replace(/,[s]*/g, ", ")}</td>
                                         <td>{item.attire}</td>
                                     </tr>
                                 );
